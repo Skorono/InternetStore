b993b36c82fb2c11803a814bc3a4a6b9a098fdab
skoron <astat.popov@yandex.ru>
2023-04-28 02:39:20 +0300

    Изменения приложения и БД

---
+++ Курсовой проект БД/PROCEDURE/AddUser.sql
@@ -0,0 +1,19 @@
+USE InternetStore;
+
+GO
+CREATE PROCEDURE AddUser
+	@email varchar(256),
+	@password varchar(max),
+	@name varchar(max),
+	@surname varchar(max) = '',
+	@middle_name varchar(max) = '',
+	@photo varbinary(max) = NULL,
+	@phone_number varchar(12) = NULL,
+	@role char(1) = 'U'
+AS
+	INSERT INTO [User](email, [password], role_id) VALUES(@email, @password, @role);
+	INSERT INTO UserPersonalInf(id, name, surname, middle_name, photo, phone_number)
+		VALUES((SELECT id FROM [User] WHERE email=@email), @name, @surname, @middle_name, @photo, @phone_number);
+GO
+
+EXEC AddUser @email='astat.popov@yandex.ru', @password='233833', @name='Михаил';
