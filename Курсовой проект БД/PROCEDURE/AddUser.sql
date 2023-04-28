897d1ff84635cd9f8e6ffffaa3b9cd5fdc6900d2
skoron ▌ <astat.popov@yandex.ru>
2023-04-28 09:33:22 +0300

    .

--- Курсовой проект БД/PROCEDURE/AddUser.sql
+++ Курсовой проект БД/PROCEDURE/AddUser.sql
@@ -13,7 +13,16 @@ CREATE PROCEDURE AddUser
 AS
 	INSERT INTO [User](email, [password], role_id) VALUES(@email, @password, @role);
 	INSERT INTO UserPersonalInf(id, name, surname, middle_name, photo, phone_number)
-		VALUES((SELECT id FROM [User] WHERE email=@email), @name, @surname, @middle_name, @photo, @phone_number);
+		VALUES(
+				(
+					SELECT TOP 1 
+						id 
+					FROM [User]
+					ORDER BY id DESC
+				), 
+				@name, @surname, @middle_name, @photo, @phone_number);
 GO
 
-EXEC AddUser @email='astat.popov@yandex.ru', @password='233833', @name='Михаил';
+EXEC AddUser @email='astat.popov@yandex.ru', @password='233833', @name='Михаил'
+EXEC AddUser @email='free-2staff@yandex.ru', @password='233833', @name='Ольга'
+
