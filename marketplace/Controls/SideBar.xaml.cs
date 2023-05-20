ad6f901669bbcc458067a073f3caf89a482f1a70
skoron <astat.popov@yandex.ru>
2023-05-20 13:37:50 +0300

    Добавлен выпадающий список категорий и подкатегорий

---
+++ marketplace/Controls/SideBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using InternetStore.ModelDB;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace InternetStore.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для SideBar.xaml
+    /// </summary>
+    public partial class SideBar : UserControl
+    {
+        List<CategoryDetails> categoriesList = new();
+        public SideBar()
+        {
+            InitializeComponent();
+            foreach (var category in BaseProvider.DbContext.Categories.ToList())
+            {
+                CategoryDetails categoryTextBlock = new CategoryDetails(category);
+                categoriesList.Add(categoryTextBlock);
+            }
+            CategoryList.ItemsSource = categoriesList;
+        }
+
+    }
+}
