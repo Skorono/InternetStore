24827babd946e7c89934c44924d319e399494367
Skorono <58297559+Skorono@users.noreply.github.com>
2023-05-30 16:36:47 +0300

    Delete PROCEDURE directory

--- PROCEDURE/CREATE/VIEW/CreateUserDTO.sql
+++
@@ -1,8 +0,0 @@
-USE InternetStore;
-
-GO
-CREATE VIEW UserViewDTO
-AS
-	SELECT [User].id, email, password, role_id, [name], surname, middle_name, photo, phone_number FROM [User]
-	INNER JOIN UserPersonalInf ON [User].id = UserPersonalInf.id
-GO
