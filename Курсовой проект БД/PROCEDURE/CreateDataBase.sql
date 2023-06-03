1f124fee3a9e4dd00ba9e13850b3c3763045c9c2
skoron <astat.popov@yandex.ru>
2023-06-03 07:10:04 +0300

    Обновлен скрипт на заполнение таблиц

--- Курсовой проект БД/PROCEDURE/CreateDataBase.sql
+++ Курсовой проект БД/PROCEDURE/CreateDataBase.sql
@@ -318,6 +318,7 @@ BEGIN
 	INSERT INTO Role(id, role_name) VALUES('R', 'Retailer');
 END
 
+USE [InternetStore]
 GO
 SET IDENTITY_INSERT [dbo].[Category] ON 
 GO
@@ -344,37 +345,63 @@ GO
 INSERT [dbo].[Category] ([id], [category_name]) VALUES (11, N'Аксессуары и услуги
 ')
 GO
+INSERT [dbo].[Category] ([id], [category_name]) VALUES (12, N'Книги')
+GO
+INSERT [dbo].[Category] ([id], [category_name]) VALUES (13, N'Игры и консоли')
+GO
+INSERT [dbo].[Category] ([id], [category_name]) VALUES (14, N'Канцелярские товары')
+GO
 SET IDENTITY_INSERT [dbo].[Category] OFF
 GO
 SET IDENTITY_INSERT [dbo].[SubCategories] ON 
 GO
-INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (1, 1, N'Процессоры ', N'{}')
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (1, 1, N'Процессоры ', N'{"Модель": null, "Сокет": null}')
 GO
 INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (2, 1, N'Видеокарты', N'{}')
 GO
 INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (3, 1, N'Материнские платы', N'{}')
 GO
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (4, 13, N'Игровые консоли', N'{}')
+GO
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (5, 13, N'Игры для PlayStation 4', N'{}')
+GO
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (6, 13, N'Игры для PlayStation 5', N'{}')
+GO
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (7, 13, N'Консоли', N'{}')
+GO
+INSERT [dbo].[SubCategories] ([id], [category_id], [name], [attributes]) VALUES (9, 12, N'Развитие ребенка', N'{}')
+GO
 SET IDENTITY_INSERT [dbo].[SubCategories] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Product] ON 
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1, 1, N'Процессор AMD Ryzen 5 4600G, SocketAM4, BOX [100-100000147box]', N'{"count":100, "cost": 10990}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1, 1, N'Процессор AMD Ryzen 5 4600G, SocketAM4, BOX [100-100000147box]', N'{"Модель": "AMD PRO A6-8570", "Сокет": "AM4", "count": 100, "cost": 10990, "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (2, 1, N'Процессор Intel Core i7 9700, LGA 1151v2, OEM', N'{"count":99, "cost": 27990}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (2, 1, N'Процессор Intel Core i7 9700, LGA 1151v2, OEM', N'{"count":99, "cost": 27990,  "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (8, 2, N'Видеокарта KFA2 GeForce 210 [21GGF4HI00NK] [PCI-E 2.0 1 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 520 МГц]', N'{"count":250, "cost": 2699}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (8, 2, N'Видеокарта KFA2 GeForce 210 [21GGF4HI00NK] [PCI-E 2.0 1 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 520 МГц]', N'{"count":250, "cost": 2699,  "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (9, 2, N'Видеокарта KFA2 GeForce GT 710 [71GPF4HI00GK] [PCI-E 2.0 2 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 954 МГц]', N'{"count":350, "cost": 3399}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (9, 2, N'Видеокарта KFA2 GeForce GT 710 [71GPF4HI00GK] [PCI-E 2.0 2 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 954 МГц]', N'{"count":350, "cost": 3399, "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (10, 2, N'Видеокарта MSI GeForce 210 [N210-1GD3/LP] [PCI-E 2.0 1 ГБ GDDR3, 64 бит, DVI-I, HDMI, VGA (D-Sub), GPU 460 МГц]', N'{"count":550, "cost": 4199}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (10, 2, N'Видеокарта MSI GeForce 210 [N210-1GD3/LP] [PCI-E 2.0 1 ГБ GDDR3, 64 бит, DVI-I, HDMI, VGA (D-Sub), GPU 460 МГц]', N'{"count":550, "cost": 4199,  "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (11, 2, N'Видеокарта KFA2 GeForce GT 730 [73GQF8HX00HK] [PCI-E 2.0 4 ГБ DDR3, 128 бит, DVI-I, HDMI, VGA (D-Sub), GPU 700 МГц]', N'{"count":10, "cost": 4299}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (11, 2, N'Видеокарта KFA2 GeForce GT 730 [73GQF8HX00HK] [PCI-E 2.0 4 ГБ DDR3, 128 бит, DVI-I, HDMI, VGA (D-Sub), GPU 700 МГц]', N'{"count":10, "cost": 4299,  "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (12, 2, N'Видеокарта GIGABYTE GeForce GT 710 Silent LP [GV-N710D5SL-2GL] [PCI-E 2.0 2 ГБ GDDR5, 64 бит, DVI-I, HDMI, GPU 954 МГц]', N'{"count":199, "cost": 4999}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (12, 2, N'Видеокарта GIGABYTE GeForce GT 710 Silent LP [GV-N710D5SL-2GL] [PCI-E 2.0 2 ГБ GDDR5, 64 бит, DVI-I, HDMI, GPU 954 МГц]', N'{"count":199, "cost": 4999, "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (13, 2, N'Видеокарта MSI GeForce GT 730 [N730K-4GD3/OCV1] [PCI-E 2.0 4 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 902 МГц]', N'{"count":99, "cost": 7799}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (13, 2, N'Видеокарта MSI GeForce GT 730 [N730K-4GD3/OCV1] [PCI-E 2.0 4 ГБ GDDR3, 64 бит, DVI-D, HDMI, VGA (D-Sub), GPU 902 МГц]', N'{"count":99, "cost": 7799, "image": null}')
 GO
-INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (14, 3, N'Материнская плата Esonic G41CPL3 [LGA 775, Intel G41, 2xDDR3-1066 МГц, 1xPCI-Ex16, Micro-ATX]', N'{"count":1500, "cost": 2999}')
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (14, 3, N'Материнская плата Esonic G41CPL3 [LGA 775, Intel G41, 2xDDR3-1066 МГц, 1xPCI-Ex16, Micro-ATX]', N'{"count":1500, "cost": 2999, "image": null}')
+GO
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1005, 7, N'Игровая консоль PlayStation 5 Blu-Ray Japan Edition CFI-1100A, белый', N'{"count":54490, "cost": 54490, "image": null}')
+GO
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1006, 7, N'Игровая приставка PlayStation 4 Fat (500gb) Black, ps4, 1 геймпад, reseller', N'{"count":25060, "cost": 25060, "image": null}')
+GO
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1007, 7, N'Шлем виртуальной реальности Sony PlayStation VR 2', N'{"count":52755, "cost": 52755, "image": null}')
+GO
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1008, 7, N'Игровая консоль Microsoft Xbox Series S, белый', N'{"count":1500, "cost": 27813, "image": null}')
+GO
+INSERT [dbo].[Product] ([id], [subcategory_id], [product_name], [properties]) VALUES (1010, 9, N'100 заданий для малыша. 1+ | Дмитриева Валентина Геннадьевна', N'{"count":0, "cost": 267, "image": null}')
 GO
 SET IDENTITY_INSERT [dbo].[Product] OFF
 GO
