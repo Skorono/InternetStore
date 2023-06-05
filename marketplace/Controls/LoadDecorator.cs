b20ebb2f3cf25f1bcd2df6352ec3769db0a6d97c
skoron <astat.popov@yandex.ru>
2023-06-05 05:26:31 +0300

    Обновлен поиск

--- marketplace/Controls/LoadDecorator.cs
+++ marketplace/Controls/LoadDecorator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using WpfAnimatedGif;
 
 namespace InternetStore.Controls
@@ -16,29 +18,44 @@ namespace InternetStore.Controls
 
         }
 
-        public void Load<T>(T layout, Delegate Func, params object[] args) where T : Grid
+        [STAThread]
+        async public void Load<T>(T layout, Delegate Func, params object[] args) where T : Grid
         {
-
+            //Thread.Sleep(5000);
+       
             Image img = new();
             var image = new BitmapImage();
             image.BeginInit();
-            image.UriSource = new Uri(Path.Combine(Environment.GetEnvironmentVariable("Images")!, "loading.gif"), UriKind.Relative);
+            image.UriSource = new Uri(Path.Combine(Environment.GetEnvironmentVariable("Images")!, "loading3.gif"), UriKind.Relative);
             image.EndInit();
-            Grid.SetColumn(img, layout.RowDefinitions.Count - 1);
-            Grid.SetRow(img, layout.ColumnDefinitions.Count - 1);
+            Grid.SetColumn(img, layout.RowDefinitions.Count / 2);
+            Grid.SetRow(img, layout.ColumnDefinitions.Count / 2);
             ImageBehavior.SetAnimatedSource(img, image);
             layout.Children.Add(img);
-            img.UpdateLayout();
-            Console.WriteLine(img.ActualHeight);
 
-
-
-            Thread.Sleep(500);
-
+            await Task.Delay(1200);
             Func.DynamicInvoke(args);
-
             layout.Children.Remove(img);
-            layout.UpdateLayout();
+        }
+
+        public static Task StartSTATask(Action func)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    func();
+                    tcs.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            return tcs.Task;
         }
 
         /*  private TResult Foo<TResult>(Delegate f, params object[] args)
