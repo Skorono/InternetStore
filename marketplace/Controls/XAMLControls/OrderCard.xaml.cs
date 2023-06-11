a2dca115f8af6d1e40a2e4f0b402c17f0bd2bbab
skoron <astat.popov@yandex.ru>
2023-06-11 03:43:52 +0300

    Обновлен список заказов

--- marketplace/Controls/XAMLControls/OrderCard.xaml.cs
+++ marketplace/Controls/XAMLControls/OrderCard.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using InternetStore.ModelDB;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace InternetStore.Controls.XAMLControls
@@ -15,6 +19,8 @@ namespace InternetStore.Controls.XAMLControls
             this.DoubleClick += ToOrderPage;
             DateOfForm = OrderModel.DatetimeOfForm;
             InitializeComponent();
+            CreateStatusButton();
+            CreateOrderComposition();
         }
 
         private void CreateStatusButton()
@@ -33,6 +39,21 @@ namespace InternetStore.Controls.XAMLControls
         private void ToOrderPage(object sender, System.Windows.RoutedEventArgs e)
         {
             //NavigationService.GetNavigationService().Navigate(new OrderPage(OrderModel.Id));
+        }
+
+        private void CreateOrderComposition()
+        {   
+            List<TextBlock> ProductsName = new List<TextBlock>();
+            List<OrderDetail> OrderDetails = BaseProvider.DbContext.OrderDetails.Where(raw => raw.OrderId == OrderModel.OrderId).ToList();
+
+            foreach (var orderRaw in OrderDetails)
+                foreach (var product in BaseProvider.DbContext.Products.Where(p => p.Id == orderRaw.ProductId)) {
+                    TextBlock ProductName = new TextBlock();
+                    ProductName.Text = product.ProductName;
+                    ProductName.TextTrimming = System.Windows.TextTrimming.CharacterEllipsis;
+                    ProductsName.Add(ProductName);
+                }
+            OrderComposition.ItemsSource = ProductsName;
         }
 
         private void Payment(object sender, System.Windows.RoutedEventArgs e)
